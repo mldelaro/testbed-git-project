0bd0ee9a6f9fe58ca0430e1ef98cac78f7542417
Matthew de la Rosa <mldelaro@asu.edu>
2017-05-16 14:29:56 -0700

    Make change from new branch

--- src/edu/asu/mldelaro/testbed/Main.java
+++ src/edu/asu/mldelaro/testbed/Main.java
@@ -3,7 +3,7 @@ package edu.asu.mldelaro.testbed;
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello Git! Some Change");
+		System.out.println("Hello Git! Some change made from a new branch");
 		
 		System.out.println("Continue making changes");
 		System.out.println("More changes from master!");

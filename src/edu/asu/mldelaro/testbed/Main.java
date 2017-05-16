6603be7e412b36a71882dadb74e8145f9552ab06
Matthew de la Rosa <mldelaro@asu.edu>
2017-05-16 14:44:40 -0700

    Add another print line from master

--- src/edu/asu/mldelaro/testbed/Main.java
+++ src/edu/asu/mldelaro/testbed/Main.java
@@ -7,6 +7,7 @@ public class Main {
 		
 		System.out.println("Continue making changes");
 		System.out.println("More changes from master!");
+		System.out.println("More changes from master!");
 	}
 
 }

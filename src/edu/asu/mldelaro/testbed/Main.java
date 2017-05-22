9f39a132f7c10ae5f548fb57a83f732439e1321d
Matthew de la Rosa <mldelaro@asu.edu>
2017-05-22 13:43:45 -0700

    3 - Resolve merge conflict on 'some-branch rebase on main'

--- src/edu/asu/mldelaro/testbed/Main.java
+++ src/edu/asu/mldelaro/testbed/Main.java
@@ -3,8 +3,7 @@ package edu.asu.mldelaro.testbed;
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello Git!");
-		
 		System.out.println("Hello From master!");
+		System.out.println("Hello Git! Hello from branch");
 	}
 }

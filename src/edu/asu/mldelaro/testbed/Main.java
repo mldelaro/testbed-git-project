b954dc27f96538c9127526aba488da90c8e2b2ac
Matthew de la Rosa <mldelaro@asu.edu>
2017-05-22 12:52:34 -0700

    Delete more lines

--- src/edu/asu/mldelaro/testbed/Main.java
+++ src/edu/asu/mldelaro/testbed/Main.java
@@ -4,8 +4,5 @@ public class Main {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello Git! Another change from branch");
-		
-		System.out.println("Continue making changes");
 	}
-
 }

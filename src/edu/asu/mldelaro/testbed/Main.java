76827fc264956a0334ee6ea39c00e515a64ecfac
Matthew de la Rosa <mldelaro@asu.edu>
2017-05-22 13:27:11 -0700

    1-change made to branch

--- src/edu/asu/mldelaro/testbed/Main.java
+++ src/edu/asu/mldelaro/testbed/Main.java
@@ -3,6 +3,6 @@ package edu.asu.mldelaro.testbed;
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello Git! Another change from branch");
+		System.out.println("Hello Git!");
 	}
 }

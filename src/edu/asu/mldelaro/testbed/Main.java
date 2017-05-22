bdabe165edb18a6cd4ca995448ed03d7ab9ceb39
Matthew de la Rosa <mldelaro@asu.edu>
2017-05-22 10:49:06 -0700

    Remove a print line

--- src/edu/asu/mldelaro/testbed/Main.java
+++ src/edu/asu/mldelaro/testbed/Main.java
@@ -7,7 +7,6 @@ public class Main {
 		
 		System.out.println("Continue making changes");
 		System.out.println("More changes from master!");
-		System.out.println("More changes from master!");
 	}
 
 }

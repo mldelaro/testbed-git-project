f8518b0b96e854d7e219ed1be77f254e7c0a4342
Matthew de la Rosa <mldelaro@asu.edu>
2017-05-22 10:51:25 -0700

    Delete change from branch

--- src/edu/asu/mldelaro/testbed/Main.java
+++ src/edu/asu/mldelaro/testbed/Main.java
@@ -3,7 +3,7 @@ package edu.asu.mldelaro.testbed;
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello Git! Some change made from a new branch");
+		System.out.println("Hello Git!");
 		
 		System.out.println("Continue making changes");
 		System.out.println("More changes from master!");

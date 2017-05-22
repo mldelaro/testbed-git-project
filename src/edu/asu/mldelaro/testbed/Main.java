9085be652f4770b4af528971d93e43055b5cf0d1
Matthew de la Rosa <mldelaro@asu.edu>
2017-05-22 13:44:40 -0700

    3 - more changes to branch

--- src/edu/asu/mldelaro/testbed/Main.java
+++ src/edu/asu/mldelaro/testbed/Main.java
@@ -5,5 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello From master!");
 		System.out.println("Hello Git! Hello from branch");
+		System.out.println("More from branch");
 	}
 }

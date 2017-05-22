aca3fa2b0ae99f02f133ec85d0252b4f2b486092
Matthew de la Rosa <mldelaro@asu.edu>
2017-05-22 13:56:55 -0700

    RESET

--- src/edu/asu/mldelaro/testbed/Main.java
+++ src/edu/asu/mldelaro/testbed/Main.java
@@ -3,8 +3,6 @@ package edu.asu.mldelaro.testbed;
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello From master!");
-		System.out.println("Hello Git! Hello from branch");
-		System.out.println("More from branch");
+		System.out.println("Hello!");
 	}
 }

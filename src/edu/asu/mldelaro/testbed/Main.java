ca1607a79cf341964cd497da6556766d0b005a7e
Matthew de la Rosa <mldelaro@asu.edu>
2017-05-23 12:03:05 -0700

    3 - More changes from some-branch

--- src/edu/asu/mldelaro/testbed/Main.java
+++ src/edu/asu/mldelaro/testbed/Main.java
@@ -5,5 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello! From Master again");
 		System.out.println("Change from some-branch");
+		System.out.println("More changes from some-branch");
 	}
 }

a90f3051162f9a60b6bb6788bf946100d425817a
Matthew de la Rosa <mldelaro@asu.edu>
2017-05-23 13:34:28 -0700

    RESET

--- src/edu/asu/mldelaro/testbed/Main.java
+++ src/edu/asu/mldelaro/testbed/Main.java
@@ -3,9 +3,6 @@ package edu.asu.mldelaro.testbed;
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello! From Master again");
-		System.out.println("Change from some-branch");
-		System.out.println("More changes from some-branch");
-		System.out.println("Even more changes from some-branch");
+		System.out.println("Hello!");
 	}
 }

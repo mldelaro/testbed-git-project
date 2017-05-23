7602f33bba35e3f0a2004539084b532f676779b7
Matthew de la Rosa <mldelaro@asu.edu>
2017-05-23 13:22:18 -0700

    5 - Another change from master

--- src/edu/asu/mldelaro/testbed/Main.java
+++ src/edu/asu/mldelaro/testbed/Main.java
@@ -3,6 +3,6 @@ package edu.asu.mldelaro.testbed;
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello! From Master");
+		System.out.println("Hello! From Master again");
 	}
 }
